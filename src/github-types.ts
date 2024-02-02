40df1ade9159911b8ea6444921d22501ef085d90
アレクサンダー.eth <gpg@pavlovcik.com>
2024-02-02 23:53:36 +0900

    refactor: use more robust type selection

--- src/github-types.ts
+++ src/github-types.ts
@@ -1,5 +1,5 @@
-import { Endpoints } from "@octokit/types";
+import { RestEndpointMethodTypes } from "@octokit/rest";
 
-export type GitHubIssue = Endpoints["GET /repos/{owner}/{repo}/issues"]["response"]["data"][0];
-export type GitHubComment = Endpoints["GET /repos/{owner}/{repo}/issues/{issue_number}/comments"]["response"]["data"][0];
-export type GitHubLabel = Endpoints["GET /repos/{owner}/{repo}/labels"]["response"]["data"][0];
+export type GitHubIssue = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
+export type GitHubComment = RestEndpointMethodTypes["issues"]["listComments"]["response"]["data"][0];
+export type GitHubLabel = RestEndpointMethodTypes["issues"]["listLabelsOnIssue"]["response"]["data"][0];
